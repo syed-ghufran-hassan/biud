cf88ed0338a9be51cdb17b5613933fa8b2a954da
phessophissy <phessophissy@gmail.com>
2026-01-25 07:52:14 +0100

    feat(ui): add inner ear details

--- frontend/src/components/FloatingCat.tsx
+++ frontend/src/components/FloatingCat.tsx
@@ -5,7 +5,9 @@ export default function FloatingCat() {
     <div className="animate-float relative w-32 h-32">
       {/* Cat Ears */}
       <div className="absolute -top-4 left-2 w-0 h-0 border-l-[15px] border-r-[15px] border-b-[25px] border-l-transparent border-r-transparent border-b-cat-orange"></div>
+      <div className="absolute -top-3 left-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[18px] border-l-transparent border-r-transparent border-b-cat-nose"></div>
       <div className="absolute -top-4 right-2 w-0 h-0 border-l-[15px] border-r-[15px] border-b-[25px] border-l-transparent border-r-transparent border-b-cat-orange"></div>
+      <div className="absolute -top-3 right-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[18px] border-l-transparent border-r-transparent border-b-cat-nose"></div>
       {/* Cat Face */}
       <div className="absolute inset-0 bg-cat-orange rounded-full"></div>
     </div>
